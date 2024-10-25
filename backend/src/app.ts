324cab7325bf453b5c000c220c2b1174fdabac13
Sergey Chama <sergeychama24@gmail.com>
2024-10-26 00:52:36 +0300

    share public directory with client

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
@@ -14,10 +15,10 @@ mongoose.connect(DB_ADDRESS)
     console.log(err);
   });
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(router);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
