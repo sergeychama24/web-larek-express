8d46971135afb5780f81330e6031553f6b2fe722
Sergey Chama <sergeychama24@gmail.com>
2024-10-26 16:17:14 +0300

    add logger for all routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import productRouter from './product';
 import orderRouter from './order';
 import NotFoundError from '../errors/not-found-error';
+import { errorLogger, requestLogger } from '../middlewares/logger';
 
 const router = Router();
 
-router.use('/product', productRouter);
-router.use('/order', orderRouter);
-router.use('/*', () => {
+router.use('/product', requestLogger, productRouter, errorLogger);
+router.use('/order', requestLogger, orderRouter, errorLogger);
+router.use('/*', requestLogger, () => {
   throw new NotFoundError();
-});
+}, errorLogger);
 
 export default router;
