2eccc7e6af8ed81a37365124211d499f759580ee
Sergey Chama <sergeychama24@gmail.com>
2024-10-27 00:05:34 +0300

    add check _id products in order request

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -1,10 +1,19 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { faker } from '@faker-js/faker';
+import Product from '../models/product';
+import BadRequestError from '../errors/bad-request-error';
 
-export const createOrder = (req: Request, res: Response) => {
-  const { total } = req.body;
-  res.status(201).send({
-    id: faker.string.uuid(),
-    total,
-  });
+export const createOrder = (req: Request, res: Response, next: NextFunction) => {
+  const { total, items } = req.body;
+  const uniqueItems = [...new Set(items)];
+  Product.find({ _id: { $in: uniqueItems } })
+    .then(() => {
+      res.status(201).send({
+        id: faker.string.uuid(),
+        total,
+      });
+    })
+    .catch(() => {
+      next(new BadRequestError());
+    });
 };
